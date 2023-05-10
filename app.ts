364d88e2ce289e2bb51ff0b17373463ceda0e5f8
IsoDevMate <oumaoduor5827@gmail.com>
2023-05-10 21:36:18 +0300

    developing boos app

--- app.ts
+++ app.ts
@@ -1,1 +1,3 @@
-import 
+import React from 'react',
+import { BestNovel } from './components/BestNovel',
+import './App.css'
--- components/BestNovel.tsx
+++ components/BestNovel.tsx
@@ -1,4 +1,9 @@
-import React,{useState} from 'React'
-import Button from './Button.tsx'
+import React,{useState} from 'React',
+import Button from './Button.tsx',
 import { List} from './list.js'
 
+const BestNovel = () = { // add  a state variable
+    const [newList,setNewList] = useState
+   
+    
+}
